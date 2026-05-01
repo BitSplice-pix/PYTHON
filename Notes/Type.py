1e9efba41fd1dfa424e1ed2cc4a7c01152751b78
BitSplice <salmanansari74070@gmail.com>
2026-05-01 19:20:25 +0530

    Basic Notes

---
+++ Notes/Type.py
@@ -0,0 +1,14 @@
+print ('Type Function')
+a = 10
+t = type(a)
+print(t)
+
+# we can use this for the type converion for input from the user
+
+b = input('Enter the 1 Number: ')
+c = input('Enter the 2 Number: ')
+print(b+c)  #It is taking input as string
+
+d = int(input('Enter the 1 Number: '))
+e = int(input('Enter the 2 Number: '))
+print(d+e) #It is type conerting it to integer 
